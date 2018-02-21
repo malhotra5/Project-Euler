44b81850aa7b216ae06099e364ee0a2d8cad69ca
Rohit Malhotra <rohitvinodmalhotra@gmail.com>
2018-02-21 13:06:55 -0800

    Update Multiples of 3 and 5.py

--- Project euler solutions/Multiples of 3 and 5.py
+++ Project euler solutions/Multiples of 3 and 5.py
@@ -5,27 +5,17 @@
 
 #Solution - 233168
 
-def findMultSum(rang):
-    #Making a list for multiples of 3 and 5
-    nums = []
+def findSum(rang):
     #Interating over the range in which we need to find multiples of 3 and 5
     for i in range(rang):
         #Checking if the number is a multiple of 3 or 5
         if(i%3 == 0 or i%5 == 0):
-            #If the number already exists then pass as we don't want any repeats like 15 which is a multiple of both 3 and 5
-            if i in nums:
-                pass
-            else:
-                #Put the new number in the list
-                nums.append(i)
-    sumed = 0
-    #Sum all the numbers in the list
-    for i in nums:
-        sumed = sumed + i
-    #Return sum
-    return sumed
+            #If it is, adding it to the summ of all the previous multiples of 3 and 5.
+            summ = summ + i
+            
+    return summ
 
 #Printing solution
-print(findMultSum)
+print(findSum)
 
 
